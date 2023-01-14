91dc34a2c9c6a4c2fe781c1f108963aedb3ce3be
johnny.geling <johnny@geling.be>
2023-01-14 20:58:59 +0100

    Process selected object

--- RunSimulation.py
+++ RunSimulation.py
@@ -43,69 +43,74 @@ class cmd_runMcSimulation():
         start = time.time()
         print("starting simulation at: ", start)
         Doc= App.ActiveDocument
-        FilePath = Doc.getObject("DVA_Analysis").OutputFile
-        Repet = Doc.getObject("DVA_Analysis").Sample
-        with open(FilePath, "w", newline="") as f:
-            data = csv.writer(f)
-            dele = []
-            data.writerow(dele)
-        f.close
-
-        # DVA_Points refers to points with a tolerance inside a Body
-        # in the case study
-        DvaPts = Doc.findObjects("PartDesign::Point")
-        DvaPts = dvu.listDvaPoints(DvaPts)
-
-        # Contraintes are the constraint Simulating process variation
-        Contraintes = Doc.findObjects("App::FeaturePython")
-        Contraintes = dvu.listAssyConstraints(Contraintes)
-
-        DvaPts = DvaPts + Contraintes
-        # Mesure are the measurement from Assy3 with DVA upgrade
-        Mesure = Doc.getObject("DVA_Analysis").ListPoints
-        InitialState = dvu.initialStateSave(DvaPts)
-
-        b = []
-
-        for x in np.arange(0, Repet, 1):
-            a = []
-
-            dvu.dvaPtRndVal(DvaPts, a, b, x)
-            Gui.runCommand('asm3CmdSolve',0)
-
-            if x==0:
-                dtNames = np.asarray(b)
-                dtNames = np.expand_dims(dtNames,axis=1)
-
-            dt = np.asarray(a)
-            dt = np.expand_dims(dt,axis=1)
-
-            if x==0:
-                dt2=dt
-
-            dt2 = np.append(dt2, dt, axis=1)
-            dvu.backInitialState(InitialState, Doc)
-            if Doc.getObject("DVA_Analysis").UpdateGui:
-                Gui.updateGui()
-
-            print("simulation loop:", x)
-
-        dtfull = np.concatenate((dtNames,dt2), axis=1)
-        dtfull = np.transpose(dtfull)
-        pddf = pd.DataFrame(data=dtfull[1:,1:],
-                            columns=dtfull[0,1:],
-                            dtype=float)
-        for mes in Mesure:
-            mes = Doc.getObject(mes)
-            hist = pddf[[mes.Label]].hist()
-
-        pddf.to_csv(FilePath, sep=',', header=True)
-
-        f.close
-        end = time.time()
-        print("Simulation performed in : ",timedelta(seconds=end - start))
-        App.ActiveDocument.recompute()
-
+        selected_object = Gui.Selection.getSelection()[0]
+        print(selected_object.Name)
+        if hasattr(selected_object, "AnalysisType"):
+                
+            FilePath = selected_object.OutputFile
+            Repet = selected_object.Sample
+            with open(FilePath, "w", newline="") as f:
+                data = csv.writer(f)
+                dele = []
+                data.writerow(dele)
+            f.close
+    
+            # DVA_Points refers to points with a tolerance inside a Body
+            # in the case study
+            DvaPts = Doc.findObjects("PartDesign::Point")
+            DvaPts = dvu.listDvaPoints(DvaPts)
+    
+            # Contraintes are the constraint Simulating process variation
+            Contraintes = Doc.findObjects("App::FeaturePython")
+            Contraintes = dvu.listAssyConstraints(Contraintes)
+    
+            DvaPts = DvaPts + Contraintes
+            # Mesure are the measurement from Assy3 with DVA upgrade
+            Mesure = selected_object.ListPoints
+            InitialState = dvu.initialStateSave(DvaPts)
+    
+            b = []
+    
+            for x in np.arange(0, Repet, 1):
+                a = []
+    
+                dvu.dvaPtRndVal(DvaPts, a, b, x)
+                Gui.runCommand('asm3CmdSolve',0)
+    
+                if x==0:
+                    dtNames = np.asarray(b)
+                    dtNames = np.expand_dims(dtNames,axis=1)
+    
+                dt = np.asarray(a)
+                dt = np.expand_dims(dt,axis=1)
+    
+                if x==0:
+                    dt2=dt
+    
+                dt2 = np.append(dt2, dt, axis=1)
+                dvu.backInitialState(InitialState, Doc)
+                if selected_object.UpdateGui:
+                    Gui.updateGui()
+    
+                print("simulation loop:", x)
+    
+            dtfull = np.concatenate((dtNames,dt2), axis=1)
+            dtfull = np.transpose(dtfull)
+            pddf = pd.DataFrame(data=dtfull[1:,1:],
+                                columns=dtfull[0,1:],
+                                dtype=float)
+            for mes in Mesure:
+                mes = Doc.getObject(mes)
+                hist = pddf[[mes.Label]].hist()
+    
+            pddf.to_csv(FilePath, sep=',', header=True)
+    
+            f.close
+            end = time.time()
+            print("Simulation performed in : ",timedelta(seconds=end - start))
+            App.ActiveDocument.recompute()
+        else:
+            print('selected object is not a DVA group')
         return
 
     def IsActive(self):
